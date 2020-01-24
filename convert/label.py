bb8fb4beba43eafb1ea3199ac54df9a80048d219
pahandav <34494459+pahandav@users.noreply.github.com>
2020-01-24 18:24:47 -0500

    Second label routine pass - find jump targets

--- convert/label.py
+++ convert/label.py
@@ -17,7 +17,7 @@ def tokenize_line(Lexer, untokenized_line):
     return token_list
 
 def sanity_check_listing(Lexer, numbered_file):
-    """ This function returns a list of all the line numbers in the file. """
+    """ First pass - This function returns a list of all the line numbers in the file. """
     original_line_numbers = list()
     for line in numbered_file:
         tokenized_line = tokenize_line(Lexer, line)
@@ -33,12 +33,33 @@ def sanity_check_listing(Lexer, numbered_file):
                     # Out of order check.
                     logging.warning('Line number ' + tokenized_line[0].val + ' is out of order.')
                 original_line_numbers.append(tokenized_line[0].val)
-    print(original_line_numbers)
     return original_line_numbers
+
+def extract_jump_targets(Lexer, numbered_file, original_line_numbers):
+    """ Second pass - This function returns a set of jump targets. """
+    jump_targets = set()
+    for line in numbered_file:
+        tokenized_line = tokenize_line(Lexer, line)
+        tokenized_line_length = len(tokenized_line)
+        indices = [i for i, x in enumerate(tokenized_line) if x.type == "FLOW"]
+        logging.debug(line)
+        logging.debug(indices)
+        for index in indices:
+            if index+1 < tokenized_line_length:
+                if tokenized_line[index+1].type == 'NUMBER':
+                    target = tokenized_line[index+1].val
+                    logging.debug(target)
+                    if target not in original_line_numbers:
+                        logging.critical('Fatal Error! Attempt to jump to line number ' + target + ' is invalid!')
+                        sys.exit(1)
+                    jump_targets.add(target)
+    logging.debug(jump_targets)
+    return jump_targets
 
 def label_listing(numbered_file):
     """ This function returns a labeled BASIC listing. """
     basic_type = 'generic'
     Lexer = generate_label_lexer(basic_type)
     original_line_numbers = sanity_check_listing(Lexer, numbered_file)
+    jump_targets = extract_jump_targets(Lexer, numbered_file, original_line_numbers)
     sys.exit()
