5f5c618c4dcf22908ccf364fb505430a10c79e84
pahandav <34494459+pahandav@users.noreply.github.com>
2020-01-23 21:24:40 -0500

    First label routine pass - sanity checking

--- convert/label.py
+++ convert/label.py
@@ -16,13 +16,29 @@ def tokenize_line(Lexer, untokenized_line):
         print('LexerError at position %s' % err.pos)
     return token_list
 
+def sanity_check_listing(Lexer, numbered_file):
+    """ This function returns a list of all the line numbers in the file. """
+    original_line_numbers = list()
+    for line in numbered_file:
+        tokenized_line = tokenize_line(Lexer, line)
+        if tokenized_line[0].type == 'LINE':
+            if len(original_line_numbers) == 0:
+                original_line_numbers.append(tokenized_line[0].val)
+            else:
+                if tokenized_line[0].val in original_line_numbers:
+                    # Duplicate Line sanity check
+                    logging.critical('Fatal Error! Line number ' + tokenized_line[0].val + ' is a duplicate!')
+                    sys.exit(1)
+                if int(tokenized_line[0].val) < int(original_line_numbers[-1]):
+                    # Out of order check.
+                    logging.warning('Line number ' + tokenized_line[0].val + ' is out of order.')
+                original_line_numbers.append(tokenized_line[0].val)
+    print(original_line_numbers)
+    return original_line_numbers
+
 def label_listing(numbered_file):
     """ This function returns a labeled BASIC listing. """
     basic_type = 'generic'
     Lexer = generate_label_lexer(basic_type)
-    for line in numbered_file:
-        tokenized_line = tokenize_line(Lexer, line)
-        for token in tokenized_line:
-            print(token.type)
-            print(token.val)
+    original_line_numbers = sanity_check_listing(Lexer, numbered_file)
     sys.exit()
